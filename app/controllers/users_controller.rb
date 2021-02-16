3ac129def9dd57d859948cfbb926c6fc58369eb4
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-15 20:55:19 -0700

    finished building user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user
+      @user.save
+    end
+  end
+
+  def edit
+    render json: @user
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :profession, :bio, :image, :linked_in, :git_hub, :twitter, :instagram )
+
+  end
 end
+
