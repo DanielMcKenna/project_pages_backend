0860a8c3f9397812318adc5964ee4e41658939dc
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-15 21:18:09 -0700

    finally fixed my seeded data error that I was recieving to make sure my users are being pulled

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user = User.new
+user.first_name = "Test"
+user.last_name = "Test"
+user.email = "test@test.com"
+user.password = "password"
+user.profession = "Sucking at this"
+user.bio = "Building this for fun but missed a comma and after so many guides i realized it was a comma not my code..."
+user.linked_in = "me"
+user.git_hub = "git.Git"
+user.instagram = "dang I look good"
+user.image = "image of me looking good"
+user.username = "suckit"
+user.save!
 
 puts "user created"
