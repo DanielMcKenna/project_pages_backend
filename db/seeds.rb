4fa24e8bfa1bf310cea294a51f5a4d4d5277cb4d
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-16 19:11:34 -0700

    added faker seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-user = User.new
-user.first_name = "Test"
-user.last_name = "Test"
-user.email = "test@test.com"
-user.password = "password"
-user.profession = "Sucking at this"
-user.bio = "Building this for fun but missed a comma and after so many guides i realized it was a comma not my code..."
-user.linked_in = "me"
-user.git_hub = "git.Git"
-user.instagram = "dang I look good"
-user.image = "image of me looking good"
-user.twitter = "sorry I forgot you"
-user.save!
+3.times do
+  user = User.new
+  user.first_name = Faker::JapaneseMedia::Naruto.demon
+  user.last_name = Faker::JapaneseMedia::Naruto.eye
+  user.email = Faker::Internet.email
+  user.password = "password"
+  user.profession = Faker::JapaneseMedia::Naruto.village
+  user.bio = Faker::Music::Opera.schubert
+  user.linked_in = Faker::JapaneseMedia::Naruto.character
+  user.git_hub = Faker::Music.band
+  user.instagram = Faker::Music.album
+  user.image = Faker::Music.album
+  user.twitter = Faker::Twitter.screen_name
+  user.save!
+end
 
-project_folder = ProjectFolder.new
-project_folder.name = "React on Rails"
-project_folder.number_of_proj = 1
-project_folder.user_id = 1
-project_folder.save!
+4.times do
+  project_folder = ProjectFolder.new
+  project_folder.name = Faker::TvShows::NewGirl.character
+  project_folder.number_of_proj = 1
+  project_folder.user_id = 1
+  project_folder.save!
+end
 
-
-project = Project.new
-project.name = "Project Pages"
-project.image = "home page image"
-project.description = "I wanted to create a project page then thought hey everyone needs one"
-project.project_link = "projectpages.com"
-project.git_hub = "here I am"
-project.video = "video of site"
-project.project_folder_id = 1
-project.save!
+6.times do
+  project = Project.new
+  project.name = Faker::TvShows::NewGirl.character
+  project.image = Faker::Food.dish
+  project.description = Faker::TvShows::NewGirl.quote
+  project.project_link = Faker::Food.dish
+  project.git_hub = Faker::Food.dish
+  project.video = Faker::Food.dish
+  project.project_folder_id = 1
+  project.save!
+end
 
 
 puts "user created"
