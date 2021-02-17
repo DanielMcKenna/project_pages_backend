522ed9bc5d4e62fd3714215528ee3cc6ad43f4a0
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-16 17:54:54 -0700

    working on finishing my controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,42 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project
+      @project.save
+    end
+  end
+
+  def edit
+    render json: @project
+  end
+
+  def update
+    @user.update(project_params)
+  end
+
+  def destroy
+    @project.destroy
+  end
+
+  private
+
+  def set_project
+    @uproject= Project.find_by_id(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :image, :description, :project_link, :git_hub, :video, )
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :profession, :bio, :image, :linked_in, :git_hub, :twitter, :instagram )
-
+  
   end
 end
 
